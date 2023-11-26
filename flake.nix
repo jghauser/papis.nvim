103b990784d0e615a30c5028b029d2cede90eec3
jghauser <julianhauser.com>
2023-11-26 14:47:55 +0100

    build: small improvement flake + reformat

--- flake.nix
+++ flake.nix
@@ -6,45 +6,52 @@
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, devenv, ... } @ inputs:
-    let
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
-    in
-    {
-      devShells = forAllSystems
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-            };
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  packages = [
-                    pkgs.sqlitebrowser
-                    pkgs.yq-go
-                  ];
-                  languages.lua = {
-                    enable = true;
-                    package = pkgs.luajit;
-                  };
-                  scripts.run-tests.exec = ''
-                    # nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedDirectory tests/spec"
-                    find tests/spec -type f -exec nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedFile {}" \;
-                  '';
-                  scripts.run-test.exec = ''
-                    nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedFile $1" \;
-                  '';
-                  scripts.run-app.exec = ''
-                    nvim -u tests/minimal_init.lua -c "lua _Load_papis()"
-                  '';
-                }
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    ...
+  } @ inputs: let
+    systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = f:
+      builtins.listToAttrs (map (name: {
+          inherit name;
+          value = f name;
+        })
+        systems);
+  in {
+    devShells =
+      forAllSystems
+      (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = [
+                pkgs.sqlitebrowser
+                pkgs.yq-go
               ];
-            };
-          });
-    };
+              languages.lua = {
+                enable = true;
+                package = pkgs.luajit;
+              };
+              scripts.run-tests.exec = ''
+                # nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedDirectory tests/spec"
+                find tests/spec -type f -exec nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedFile {}" \;
+              '';
+              scripts.run-test.exec = ''
+                nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedFile $1" \;
+              '';
+              scripts.run-app.exec = ''
+                nvim -u tests/minimal_init.lua -c "lua _Load_papis()" -c "e test.md"
+              '';
+            }
+          ];
+        };
+      });
+  };
 }
