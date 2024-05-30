ea17cf40786b45b49dd24a6111451dd002ca9fe0
jghauser <julianhauser.com>
2024-05-30 16:16:00 +0200

    flake: use unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Papis.nvim development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {
