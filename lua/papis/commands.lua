20026a4278ad9f784e5b0754b1ecf6a732af09ee
jghauser <julianhauser.com>
2024-06-02 15:00:12 +0200

    feat(commands): make all commands buffer local

--- lua/papis/commands.lua
+++ lua/papis/commands.lua
@@ -82,13 +82,13 @@ function M.setup(module)
     for module_name, module_commands in pairs(commands) do
       if config["enable_modules"][module_name] then
         for _, command in pairs(module_commands) do
-          api.nvim_create_user_command(command["name"], command["command"], command["opts"])
+          api.nvim_buf_create_user_command(0, command["name"], command["command"], command["opts"])
         end
       end
     end
   else
     for _, command in pairs(commands[module]) do
-      api.nvim_create_user_command(command["name"], command["command"], command["opts"])
+      api.nvim_buf_create_user_command(0, command["name"], command["command"], command["opts"])
     end
   end
 end
