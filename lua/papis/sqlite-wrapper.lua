d41100a1c3491ec0a396fa7c90d0649f3a7fde20
jghauser <julianhauser.com>
2024-07-29 15:21:49 +0200

    fix(sqlite-wrapper): set required to `true` not string `"true"`

--- lua/papis/sqlite-wrapper.lua
+++ lua/papis/sqlite-wrapper.lua
@@ -179,7 +179,7 @@ local schemas = {
       reference = "data.id",
       on_update = "cascade",
       on_delete = "cascade",
-      required = "true",
+      required = true,
     },
   },
   state = {
