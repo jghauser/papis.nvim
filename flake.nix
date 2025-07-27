283396716ed6d54cce7073e925dfefbd9a35c525
jghauser <julianhauser.com>
2025-07-27 14:10:01 +0200

    ci: improve nvim-test to handle various plugins

--- flake.nix
+++ flake.nix
@@ -59,9 +59,88 @@
                     in
                     # bash
                     ''
+                      # Default values
+                      COMPLETION_PROVIDER="blink"
+                      SEARCH_PROVIDER="snacks"
+                      LOAD_PAPIS=true
+                      RM_DB=false
+
+                      # Parse command line arguments
+                      while [[ $# -gt 0 ]]; do
+                        case $1 in
+                          --completion|-c)
+                            COMPLETION_PROVIDER="$2"
+                            shift 2
+                            ;;
+                          --search|-s)
+                            SEARCH_PROVIDER="$2"
+                            shift 2
+                            ;;
+                          --no-papis|-n)
+                            LOAD_PAPIS=false
+                            shift 1
+                            ;;
+                          --rm-db|-r)
+                            RM_DB=true
+                            shift 1
+                            ;;
+                          --help|-h)
+                            echo "Usage: nvim-test [options]"
+                            echo "Options:"
+                            echo "  -c, --completion ENGINE    Completion engine (cmp, blink)"
+                            echo "  -s, --search ENGINE        Picker engine (telescope, snacks)"
+                            echo "  -n, --no-papis             Do not load papis.nvim"
+                            echo "  -r, --rm-db                Clear the papis.nvim db before start"
+                            echo "  -h, --help                 Show this help"
+                            exit 0
+                            ;;
+                          *)
+                            echo "Unknown option: $1"
+                            echo "Use --help for usage information"
+                            exit 1
+                            ;;
+                        esac
+                      done
+
+                      # Validate engines
+                      case "$COMPLETION_PROVIDER" in
+                        cmp|blink) ;;
+                        *)
+                          echo "Invalid completion engine: $COMPLETION_PROVIDER"
+                          echo "Valid options: cmp, blink"
+                          exit 1
+                          ;;
+                      esac
+
+                      case "$SEARCH_PROVIDER" in
+                        telescope|snacks) ;;
+                        *)
+                          echo "Invalid picker engine: $SEARCH_PROVIDER"
+                          echo "Valid options: telescope, snacks"
+                          exit 1
+                          ;;
+                      esac
+
+                      echo "Using completion engine: $COMPLETION_PROVIDER"
+                      echo "Using picker engine: $SEARCH_PROVIDER"
+                      echo "Loading papis.nvim: $LOAD_PAPIS"
+
+                      if [ "$RM_DB" = true ]; then
+                        echo "Removing papis.nvim database..."
+                        ${pkgs.coreutils}/bin/rm -f ~/.cache/nvim-papis.nvim/papis_db/papis-nvim-test.sqlite3
+                      fi
+
+                      # Set up test environment
                       ${pkgs.coreutils}/bin/rm -rf ${test-lib-dir}
-                      ${pkgs.coreutils}/bin/cp -r -r ./tests/files/library ${test-lib-dir}
-                      ${pkgs.neovim-with-plugin}/bin/nvim -c "lua _Load_papis()" -c "e test.md"
+                      ${pkgs.coreutils}/bin/cp -r ./tests/files/library ${test-lib-dir}
+
+                      # Export configuration as environment variables
+                      export PAPIS_TEST_COMPLETION="$COMPLETION_PROVIDER"
+                      export PAPIS_TEST_SEARCH="$SEARCH_PROVIDER"
+                      export PAPIS_TEST_LOAD_PAPIS="$LOAD_PAPIS"
+
+                      echo "Starting Neovim..."
+                      ${pkgs.neovim-with-plugin}/bin/nvim -c "e test.md"
                     '';
                 };
               in
--- nix/ci-overlay.nix
+++ nix/ci-overlay.nix
@@ -1,4 +1,3 @@
-# Add flake.nix test inputs as arguments here
 {
   self,
   name,
@@ -9,16 +8,6 @@ let
     nvim:
     with final;
     let
-      profile-nvim = pkgs.vimUtils.buildVimPlugin {
-        name = "profile.nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "stevearc";
-          repo = "profile.nvim";
-          rev = "0ee32b7aba31d84b0ca76aaff2ffcb11f8f5449f";
-          hash = "sha256-usyy1kST8hq/3j0sp7Tpf/1mld6RtcVABPo/ygeqzbU=";
-        };
-      };
-
       neovimConfig = neovimUtils.makeNeovimConfig {
         withPython3 = false;
         viAlias = true;
@@ -27,21 +16,107 @@ let
           luaPkgs.papis-nvim
         ];
         plugins = with vimPlugins; [
-          # telescope-nvim
-          snacks-nvim
+          # Completion engines
           nvim-cmp
-          # blink-cmp
-          (nvim-treesitter.withPlugins (
-            ps: with ps; [
-              tree-sitter-yaml
-            ]
-          ))
-          profile-nvim
+          blink-cmp
+
+          # Pickers
+          telescope-nvim
+          snacks-nvim
+
+          # Base plugins
+          (nvim-treesitter.withPlugins (ps: with ps; [ tree-sitter-yaml ]))
         ];
       };
-      runtimeDeps = [
-        yq-go
-      ];
+
+      luaConfig =
+        # lua
+        ''
+          lua << EOF
+          local o = vim.o
+          local cmd = vim.cmd
+          local fn = vim.fn
+
+          -- disable swap
+          o.swapfile = false
+
+          -- add current directory to runtimepath
+          vim.opt.runtimepath:prepend(vim.fn.getcwd())
+
+          -- Read configuration from environment
+          local completion_provider = os.getenv("PAPIS_TEST_COMPLETION")
+          local search_provider = os.getenv("PAPIS_TEST_SEARCH")
+          local load_papis = os.getenv("PAPIS_TEST_LOAD_PAPIS")
+
+          -- Completion setup
+          if completion_provider == "cmp" then
+            local cmp = require("cmp")
+            cmp.setup({
+              mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+              }),
+              sources = cmp.config.sources({
+                { name = "papis" },
+              }),
+            })
+          elseif completion_provider == "blink" then
+            local blink = require("blink.cmp")
+            blink.setup({
+              sources = {
+                per_filetype = {
+                  yaml = { "papis" }
+                },
+              },
+            })
+          end
+
+          -- Picker setup
+          if search_provider == "telescope" then
+            local telescope = require("telescope")
+            telescope.setup({
+              defaults = {
+                layout_strategy = "vertical",
+              },
+            })
+          elseif search_provider == "snacks" then
+            require("snacks").setup({
+              picker = {},
+            })
+          end
+
+          -- remap leader
+          vim.g.mapleader = " "
+
+          ---Sets up papis
+          if load_papis then
+            local db_path = vim.fn.stdpath("cache") .. "/papis_db/papis-nvim-test.sqlite3"
+
+            local default_config = {
+              enable_modules = {
+                ["debug"] = true,
+                ["testing"] = true,
+              },
+              enable_keymaps = true,
+              db_path = db_path,
+              ["search"] = {
+                provider = search_provider,
+              },
+              ["completion"] = {
+                provider = completion_provider
+              },
+            }
+            local init_result = require("papis").setup(default_config)
+
+            return init_result
+          end
+          EOF
+        '';
+
+      runtimeDeps = [ yq-go ];
     in
     final.wrapNeovimUnstable nvim (
       neovimConfig
@@ -53,112 +128,7 @@ let
           + " "
           + ''--prefix PATH : "${lib.makeBinPath runtimeDeps}"'';
         wrapRc = true;
-        neovimRcContent =
-          # lua
-          ''
-            lua << EOF
-            local o = vim.o
-            local cmd = vim.cmd
-            local fn = vim.fn
-
-            -- disable swap
-            o.swapfile = false
-
-            -- add current directory to runtimepath to have papis.nvim
-            -- be loaded from the current directory
-            vim.opt.runtimepath:prepend(vim.fn.getcwd())
-
-            -- profile.nvim
-            local should_profile = os.getenv("NVIM_PROFILE")
-            if should_profile then
-              require("profile").instrument_autocmds()
-              if should_profile:lower():match("^start") then
-                require("profile").start("*papis")
-              else
-                require("profile").instrument("*")
-              end
-            end
-            local function toggle_profile()
-              local prof = require("profile")
-              if prof.is_recording() then
-                prof.stop()
-                vim.ui.input({ prompt = "Save profile to:", completion = "file", default = "profile.json" }, function(filename)
-                  if filename then
-                    prof.export(filename)
-                    vim.notify(string.format("Wrote %s", filename))
-                  end
-                end)
-              else
-                vim.notify("Starting profiling")
-                prof.start("*papis")
-              end
-            end
-            vim.keymap.set("", "<f1>", toggle_profile)
-
-            -- cmp
-            local cmp = require("cmp")
-            cmp.setup({
-              mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-              }),
-              sources = cmp.config.sources({
-                { name = "papis" },
-              }),
-            })
-            -- blink
-            -- local blink = require("blink.cmp")
-            -- blink.setup({
-            --   sources = {
-            --     per_filetype = {
-            --       yaml = { "papis" }
-            --     },
-            --   },
-            -- })
-
-            -- telescope
-            -- local telescope = require("telescope")
-            -- telescope.setup({
-            --   defaults = {
-            --     -- so that I can see the preview even with a split screen
-            --     layout_strategy = "vertical",
-            --   },
-            -- })
-            require("snacks").setup({
-              picker = {},
-            })
-
-            -- remap leader
-            vim.g.mapleader = " "
-
-            ---Sets up papis
-            ---@param opts table? Custom configuration options
-            ---@param rm_db boolean? Remove db on startup (defaults to `true`)
-            function _Load_papis(opts, rm_db)
-              local db_path = vim.fn.stdpath("cache") .. "/papis_db/papis-nvim-test.sqlite3"
-              local default_config = {
-                enable_modules = {
-                  ["debug"] = true,
-                  ["testing"] = true,
-                },
-                enable_keymaps = true,
-                db_path = db_path,
-              }
-              local new_config = vim.tbl_deep_extend("force", default_config, opts or {})
-              local init_result = require("papis").setup(new_config)
-
-              -- remove previous db
-              if rm_db then
-                os.remove(db_path)
-              end
-
-              return init_result
-            end
-            EOF
-          '';
+        neovimRcContent = luaConfig;
       }
     );
 in
